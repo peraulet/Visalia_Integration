bb758f89ea267c62f2d9b54aecd2c99c2a8c31f8
peraulet <danielperaulet@gmail.com>
2025-07-07 14:21:01 +0200

    Update __init__.py
    Fix call

--- custom_components/visalia_energy/__init__.py
+++ custom_components/visalia_energy/__init__.py
@@ -8,10 +8,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
